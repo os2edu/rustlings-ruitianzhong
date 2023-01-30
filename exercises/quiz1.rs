bcefc12f7f2724fc43325faaba1fb294e46a36dc
ruitianzhong <zhongruitian2003@qq.com>
2023-01-30 22:04:20 +0800

    finish the `primitive_types` exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,9 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
+fn calculate_price_of_apples(number : u32) -> u32{
+    if number > 40 {
+        number
+    }else {
+        number*2
+    }
+
+}
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
