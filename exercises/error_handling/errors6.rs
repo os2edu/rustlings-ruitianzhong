451e80adc7be313315cbc7578230b31a7fc98f20
ruitianzhong <zhongruitian2003@qq.com>
2023-01-31 22:07:32 +0800

    finish generics

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,14 +8,13 @@
 
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a hint.
 
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
 enum ParsePosNonzeroError {
     Creation(CreationError),
-    ParseInt(ParseIntError)
+    ParseInt(ParseIntError),
 }
 
 impl ParsePosNonzeroError {
@@ -23,22 +22,19 @@ impl ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
     // TODO: add another error conversion function here.
-    fn from_parseint(err: ParseIntError)-> ParsePosNonzeroError{
-        ParsePosNonzeroError::ParseIntError(err)
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
     }
 }
 
-fn parse_pos_nonzero(s: &str)
-    -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
-{
+fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse();
-    if let Err(e) = x{
-        ParsePosNonzeroError::from_parseint()
+    let t = s.parse();
+    match t {
+        Err(e) => Err(ParsePosNonzeroError::from_parseint(e)),
+        Ok(x) => PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation),
     }
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
 }
 
 // Don't change anything below this line.
@@ -57,7 +53,7 @@ impl PositiveNonzeroInteger {
         match value {
             x if x < 0 => Err(CreationError::Negative),
             x if x == 0 => Err(CreationError::Zero),
-            x => Ok(PositiveNonzeroInteger(x as u64))
+            x => Ok(PositiveNonzeroInteger(x as u64)),
         }
     }
 }
